352fcb9e067e48a71723c694c8958c7dcedc8c44
benzonico <nicolas.peru@gmail.com>
2021-12-12 08:09:41 +0100

    Day 12

--- src/main/java/aoc21/Advent2021.kt
+++ src/main/java/aoc21/Advent2021.kt
@@ -3,19 +3,100 @@ package aoc21
 import utils.PuzzleInputWriter
 import java.io.File
 import java.util.*
-import kotlin.collections.ArrayList
-import kotlin.collections.HashSet
 
 
 fun main(args: Array<String>) {
     val sessionId = args[0]
     val day = Calendar.getInstance().get(Calendar.DAY_OF_MONTH)
-    val lines = File("src/main/resources/aoc21/day11_test.txt").readLines()
-//    val lines = PuzzleInputWriter(sessionId).writeDayPuzzleToFile("2021", day).readLines()
+//    val lines = File("src/main/resources/aoc21/day12_test3.txt").readLines()
+    val lines = PuzzleInputWriter(sessionId).writeDayPuzzleToFile("2021", day).readLines()
     day(lines)
 }
+typealias Path = List<String>
 
 fun day(lines: List<String>) {
+    val immutGraph = lines.map { l -> l.split("-") }.groupBy({ a -> a[0] }, { a -> a[1] })
+    val graph = immutGraph.toMutableMap()
+
+
+    for (entry in immutGraph) {
+        entry.value.forEach { v ->
+            val keyLookup = graph.get(v)
+            if (keyLookup == null) {
+                graph[v] = listOf(entry.key)
+            } else if (!keyLookup.contains(entry.key)) {
+                val toMutableList = keyLookup.toMutableList()
+                toMutableList.add(entry.key)
+                graph[v] = toMutableList
+            }
+        }
+    }
+    println(graph)
+    fun successors(name: String): List<String> {
+        return graph.getOrDefault(name, emptyList())
+    }
+    fun canVisit(p: Path, name: String): Boolean {
+        val filter = p.filter { s -> s.toCharArray().all { c -> c.isLowerCase() }}
+        return if(filter.toSet().size == filter.size) true else !p.contains(name)
+    }
+
+    // path from start  = start + path from successors
+    //
+    //
+    fun appendToPath(p:Path, s:String): List<String> {
+        val toMutableList = p.toMutableList()
+        toMutableList.add(s)
+        return toMutableList
+    }
+    fun pathFrom(name: String, currentPaths:List<Path>): List<Path> {
+        val paths = if(currentPaths.isEmpty()) listOf(listOf("start")) else currentPaths.map { p -> appendToPath(p, name) }
+        println(paths)
+        if(name == "end") {
+            return paths
+        }
+        val successors = successors(name)
+        return paths.flatMap { p ->
+            successors.filter { succ -> succ!="start" && (succ.toCharArray().all { c-> c.isUpperCase()} ||(succ.toCharArray().all { c-> c.isLowerCase()} && canVisit(p, succ))) }
+                .flatMap { succ -> pathFrom(succ, paths)} }
+    }
+
+
+/*    fun computePathFrom(name: String, paths: List<Path>): List<Path> {
+        val somePaths = emptyList<Path>().toMutableList()
+        val successors = successors(name)
+        successors.forEach { s ->
+            if (s == "start") {
+                // do nothing, back to start.
+            } else if (s == "end") {
+                paths.forEach { l ->
+                    val toMutableList = l.toMutableList()
+                    toMutableList.add(s)
+                    somePaths.add(toMutableList.toList())
+                }
+            } else {
+                paths.forEach { p ->
+                    val c = s.toCharArray()[0];
+                    if (c.isUpperCase() || (c.isLowerCase() && canVisit(p, s))) {
+                        val pathToExplore = emptyList<Path>().toMutableList()
+                        val toMutableList = p.toMutableList()
+                        toMutableList.add(s)
+                        pathToExplore.add(toMutableList.toList())
+                        somePaths.addAll(computePathFrom(s, pathToExplore.toList()))
+                    }
+                }
+            }
+        }
+        return somePaths
+    }*/
+
+//    val paths = computePathFrom("start", listOf(listOf("start")))
+    val paths = pathFrom("start", emptyList())
+    println(paths)
+    println(paths.size)
+}
+
+
+fun day11(lines: List<String>) {
     var parse = lines.map { l -> l.toCharArray().map { c -> c.toString().toInt() } }
     var matrix = parse.map { l -> l.toMutableList() }.toMutableList()
     /*var tot = 0
@@ -37,22 +118,23 @@ fun day(lines: List<String>) {
     println(step)
 
 }
-fun sync(matrix: MutableList<MutableList<Int>>):Boolean {
-    return matrix.all { l -> l.all { i->i == 0 } }
+
+fun sync(matrix: MutableList<MutableList<Int>>): Boolean {
+    return matrix.all { l -> l.all { i -> i == 0 } }
 }
 
 fun step(matrix: MutableList<MutableList<Int>>): Pair<MutableList<MutableList<Int>>, Int> {
     val toUpdate = ArrayDeque<Pair<Int, Int>>()
-    toUpdate.addAll((0..9).toList().flatMap { x -> (0..9).toList().map { y-> Pair(x,y) } })
+    toUpdate.addAll((0..9).toList().flatMap { x -> (0..9).toList().map { y -> Pair(x, y) } })
     var tot = 0
     val flashed = HashSet<Pair<Int, Int>>()
     while (!toUpdate.isEmpty()) {
         val pop = toUpdate.pop()
-        if(flashed.contains(pop)) continue
+        if (flashed.contains(pop)) continue
         val value = matrix[pop.first][pop.second]
-        val newVal = if(value + 1<10) value+1 else 0
+        val newVal = if (value + 1 < 10) value + 1 else 0
         matrix[pop.first][pop.second] = newVal
-        if(newVal == 0) {
+        if (newVal == 0) {
             tot++
             flashed.add(pop)
             (pop.first - 1..pop.first + 1).forEach { x ->
@@ -67,11 +149,11 @@ fun step(matrix: MutableList<MutableList<Int>>): Pair<MutableList<MutableList<In
 }
 
 fun day10(lines: List<String>) {
-    val scores = lines.map { l -> computeScore(l) }.filter { i -> i>0 }.sorted()
-    println(scores[scores.size/2])
+    val scores = lines.map { l -> computeScore(l) }.filter { i -> i > 0 }.sorted()
+    println(scores[scores.size / 2])
 }
 
-fun computeScore(l: String):Long {
+fun computeScore(l: String): Long {
     val open = listOf('(', '[', '{', '<')
     val close = listOf(')', ']', '}', '>')
     val score = listOf(3, 57, 1197, 25137)
@@ -79,7 +161,7 @@ fun computeScore(l: String):Long {
     var tot = 0L
     for (c in l) {
         val opening = open.indexOf(c)
-        if(opening != -1) {
+        if (opening != -1) {
             // open char
             stack.push(opening)
         } else {
@@ -87,29 +169,30 @@ fun computeScore(l: String):Long {
             val expected = stack.pop()
             if (closing != expected) {
                 //corrupted
-    //                println("found $c instead of "+ open[expected])
-    //                tot+= score[closing]
+                //                println("found $c instead of "+ open[expected])
+                //                tot+= score[closing]
                 return -1
             }
         }
 
     }
     for (index in stack) {
-        tot = tot*5 + (index+1)
+        tot = tot * 5 + (index + 1)
     }
     return tot
 }
 
 fun day9(lines: List<String>) {
 //    println( lines.flatMapIndexed{ j, l -> l.mapIndexed{ i, nb -> riskLevel(i, j, nb.toString().toInt(), lines) }}.sum())
-    val lowestPoints = lines.flatMapIndexed { y, l -> l.mapIndexed { i, nb ->
-        if(isLowest(i, y, nb.toString().toInt(), lines)) Point(i, y, nb.toString().toInt()) else null
-     }
+    val lowestPoints = lines.flatMapIndexed { y, l ->
+        l.mapIndexed { i, nb ->
+            if (isLowest(i, y, nb.toString().toInt(), lines)) Point(i, y, nb.toString().toInt()) else null
+        }
     }.filterNotNull()
     println(lowestPoints)
     val sortedBasinSize = lowestPoints.map { p -> p.basinSize(lines) }.sortedDescending()
     println(sortedBasinSize)
-    println(sortedBasinSize[0]*sortedBasinSize[1]*sortedBasinSize[2])
+    println(sortedBasinSize[0] * sortedBasinSize[1] * sortedBasinSize[2])
 }
 
 class Point(val x: Int, val y: Int, val nb: Int) {
@@ -132,7 +215,7 @@ class Point(val x: Int, val y: Int, val nb: Int) {
         return result
     }
 
-    fun basinSize(lines: List<String>) :Int{
+    fun basinSize(lines: List<String>): Int {
         println("Compute for $this")
         val explored = java.util.HashSet<Point>()
         var toVisit = ArrayDeque<Point>()
@@ -144,20 +227,21 @@ class Point(val x: Int, val y: Int, val nb: Int) {
             println("visiting $current $filter")
             filter.forEach { p -> toVisit.push(p) }
         }
-        println("$this  "+explored)
+        println("$this  " + explored)
         return explored.size
     }
-    fun adjacentPoints(lines: List<String>):List<Point> {
+
+    fun adjacentPoints(lines: List<String>): List<Point> {
         val res = ArrayList<Point>()
         // left
         val line = lines[y]
-        if(x != 0) res.add(Point(x-1 ,y, line[x-1].toString().toInt()))
+        if (x != 0) res.add(Point(x - 1, y, line[x - 1].toString().toInt()))
         //right
-        if(x < line.length-1) res.add(Point(x+1 ,y, line[x+1].toString().toInt()))
+        if (x < line.length - 1) res.add(Point(x + 1, y, line[x + 1].toString().toInt()))
         //up
-        if(y != 0) res.add(Point(x, y-1, lines[y-1][x].toString().toInt()))
+        if (y != 0) res.add(Point(x, y - 1, lines[y - 1][x].toString().toInt()))
         //down
-        if(y < lines.size-1) res.add(Point(x, y+1, lines[y+1][x].toString().toInt()))
+        if (y < lines.size - 1) res.add(Point(x, y + 1, lines[y + 1][x].toString().toInt()))
 //        println("$this....$res")
         return res
     }
@@ -167,69 +251,77 @@ class Point(val x: Int, val y: Int, val nb: Int) {
     }
 }
 
-fun isLowest(index:Int, lineNb:Int, nb:Int, lines: List<String>):Boolean {
+fun isLowest(index: Int, lineNb: Int, nb: Int, lines: List<String>): Boolean {
     val line = lines[lineNb]
     var isLowest = true
     // left
-    if(isLowest && index != 0) {
+    if (isLowest && index != 0) {
         val left = line[index - 1].toString().toInt()
         isLowest = nb < left
     }
     //right
-    if(isLowest && index < line.length-1) {
+    if (isLowest && index < line.length - 1) {
         val right = line[index + 1].toString().toInt()
-        isLowest = nb <right
+        isLowest = nb < right
     }
     //up
-    if(isLowest && lineNb != 0) {
+    if (isLowest && lineNb != 0) {
         val up = lines[lineNb - 1][index].toString().toInt()
         isLowest = nb < up
     }
     //down
-    if(isLowest && lineNb < lines.size-1) {
+    if (isLowest && lineNb < lines.size - 1) {
         val down = lines[lineNb + 1][index].toString().toInt()
         isLowest = nb < down
     }
     return isLowest
 }
-fun riskLevel(index:Int, lineNb:Int, nb:Int, lines: List<String>):Int {
-    return if(isLowest(index, lineNb, nb, lines)) nb+1 else 0;
+
+fun riskLevel(index: Int, lineNb: Int, nb: Int, lines: List<String>): Int {
+    return if (isLowest(index, lineNb, nb, lines)) nb + 1 else 0;
 }
+
 fun day8(lines: List<String>) {
     //                               1                        4                         7        8
     val numbers = listOf("abcefg", "cf", "acdeg", "acdfg", "bcdf", "abdfg", "abdefg", "acf", "abcdefg", "abcdfg")
-    val parsedLines = lines.map { s -> OneInput.create(s)}
-    println(parsedLines.map{i -> i.toNumber()}.sum())
+    val parsedLines = lines.map { s -> OneInput.create(s) }
+    println(parsedLines.map { i -> i.toNumber() }.sum())
 //    val count = flatMap.count { l -> isUniqueDigit(l.length) }
 //    println(count)
 }
-class OneInput(val digits:List<String>, val display:List<String>) {
-//                               1                        4                         7        8
+
+class OneInput(val digits: List<String>, val display: List<String>) {
+    //                               1                        4                         7        8
     val numbers = listOf("abcefg", "cf", "acdeg", "acdfg", "bcdf", "abdfg", "abdefg", "acf", "abcdefg", "abcdfg")
+
     companion object {
-        fun create(l:String):OneInput {
+        fun create(l: String): OneInput {
             val split = l.split("|")
-            return OneInput(split[0].split(" ").filter{s->s.isNotBlank()},split[1].split(" ").filter{s->s.isNotBlank()} )
+            return OneInput(
+                split[0].split(" ").filter { s -> s.isNotBlank() },
+                split[1].split(" ").filter { s -> s.isNotBlank() })
         }
     }
 
-    fun toNumber():Int{
-        val map = digits.associateBy({d->d.toCharArray().sortedArray().joinToString("")},{ d->knownDigit(d, digits) }).toMap()
+    fun toNumber(): Int {
+        val map =
+            digits.associateBy({ d -> d.toCharArray().sortedArray().joinToString("") }, { d -> knownDigit(d, digits) })
+                .toMap()
         return display.map { d -> map[d.toCharArray().sortedArray().joinToString("")] }.joinToString("").toInt()
     }
 
-    private fun knownDigit(d: String, digits: List<String>):Int {
-        if(isUniqueDigit(d.length)) {
-            return numbers.indexOfFirst { l-> l.length==d.length }
+    private fun knownDigit(d: String, digits: List<String>): Int {
+        if (isUniqueDigit(d.length)) {
+            return numbers.indexOfFirst { l -> l.length == d.length }
         }
         val findOne = digits.first { nb -> nb.length == 2 }
         val findFour = digits.first { nb -> nb.length == 4 }
-        if(d.length == 6) {
+        if (d.length == 6) {
             // 9 6 0
-            return if(containsEachChar(d, findOne)) (if(containsEachChar(d,findFour)) 9 else 0)  else 6
+            return if (containsEachChar(d, findOne)) (if (containsEachChar(d, findFour)) 9 else 0) else 6
 
         } else {
-            return if(containsEachChar(d, findOne)) 3 else if(commonWithFour(d, findFour)==2) 2 else 5
+            return if (containsEachChar(d, findOne)) 3 else if (commonWithFour(d, findFour) == 2) 2 else 5
         }
         return -1
     }
@@ -238,7 +330,7 @@ class OneInput(val digits:List<String>, val display:List<String>) {
         return d.toCharArray().filter { c -> findFour.contains(c) }.count()
     }
 
-    fun containsEachChar(s:String, sub:String):Boolean {
+    fun containsEachChar(s: String, sub: String): Boolean {
         return sub.toCharArray().all { c -> s.contains(c) }
     }
 
@@ -248,6 +340,6 @@ class OneInput(val digits:List<String>, val display:List<String>) {
 
 }
 
-fun isUniqueDigit(length: Int):Boolean {
-    return length == 2 ||length==3 ||length==4 ||length==7
+fun isUniqueDigit(length: Int): Boolean {
+    return length == 2 || length == 3 || length == 4 || length == 7
 }
